fce4d30a0996581f0c134a6a0bde4b6f8dc2a6b6
modekangkang <modekangkang@foxmail.com>
2022-04-17 20:32:35 +0800

    add util

---
+++ src/main/java/com/haibin/myzhxy/MyzhxyApplicaiton.java
@@ -0,0 +1,11 @@
+package com.haibin.myzhxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MyzhxyApplicaiton{
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyzhxyApplicaiton.class, args);
+    }
+}
